c9454ce751a0c27d22ada0f53f55351b2038a4d9
amiulaa <aminmaulanaarrosyid39@gmail.com>
2025-05-30 12:24:11 +0700

    upload

---
+++ app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+// @phpstan-ignore-next-line
+$routes->get('/', 'Home::index');
+
+
---
+++ app/Views/v_home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+    <script src="<?= base_url('assets/js/main.js') ?>"></script>
+
+</head>
+<body>
+    <h1>Halo, saya Kelvin!</h1>
+    <p>Selamat datang di website portofolio saya.</p>
+</body>
+</html>
