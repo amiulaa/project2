1346c72df3cbc4223d93fe08280608d461ea0416
amiulaa <aminmaulanaarrosyid39@gmail.com>
2025-06-20 11:41:09 +0700

    modif, add and delete

---
+++ app/Views/v_about.php
@@ -0,0 +1,54 @@
+<?= $this->extend('layouts/template-page'); ?>
+<?= $this->section('content'); ?>
+
+<!-- About Section -->
+    <section id="about" class="about section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>About</h2>
+        <p>He is a 6th semester student at Nahdlatul Ulama University of Indonesia. He does anything for his own good. There are many challenges that stand in his way, he overcomes all obstacles one by one. He was born into a simple family but that did not stop him from pursuing his dreams.</p>
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+            <img src="assets/img/ami2.jpeg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 content">
+            <h2>UI/UX Designer &amp; Web Developer.</h2>
+            <p class="fst-italic py-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>21 August 2004</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62123456789</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Bogor, West Java</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>20</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Junior</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>contohaja@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+            <p class="py-3">
+              Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+              Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+</section><!-- /About Section -->
+
+<?= $this->endSection(); ?>
