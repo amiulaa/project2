47ece154aaa87d45a78609c907ddcb3101789e24
amiulaa <aminmaulanaarrosyid39@gmail.com>
2025-06-20 11:51:08 +0700

    add errors

---
+++ app/Views/errors/not_found.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,
+initial-scale=1">
+<!-- Bootstrap CSS -->
+<link
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/b
+ootstrap.min.css" rel="stylesheet" integrity="sha384-
+1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6j
+IW3" crossorigin="anonymous">
+<title>Error 404 CI4</title>
